d8579b9db33f2d70a57536d391bc91721ae9b361
Cetin Kaya <cetin@sufle.io>
2023-07-31 19:19:58 +0300

    authentication added to the endpoints

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,6 +3,9 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
 
+import bcrypt from 'bcrypt';
+import  jwt from "jsonwebtoken";
+
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
@@ -20,5 +23,27 @@ export const AppDataSource = new DataSource({
 AppDataSource.initialize()
     .then(async () => {
         console.log("DB initialized");
+        const superUserParams = {
+            id: "1",
+            name: "superuser",
+            email: "superuser@test.io",
+            password: await bcrypt.hash("superpassword", 10)
+        }
+
+      const token = jwt.sign(
+          { user_id: superUserParams.id, email: superUserParams.email },
+        "your_secret_key_here",
+        {
+          expiresIn: "365d",
+        }
+      );
+
+        const user = new User()
+      user.id = superUserParams.id
+      user.name = superUserParams.name
+      user.email = superUserParams.email
+      user.password =   superUserParams.password
+        user.token = token
+        await AppDataSource.manager.save(user)
     })
     .catch((error) => console.log(error))
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import controller from '../controllers/users';
+import authenticateToken from '../middlewares/auth';
 
 const router = express.Router();
-router.post('/users', controller.createUser);
-router.get('/users', controller.getUsers);
-router.get('/user/:id', controller.getUser);
-router.patch('/user/:id', controller.updateUser);
-router.delete('/user/:id', controller.deleteUser);
+router.post('/users', authenticateToken, controller.createUser);
+router.get('/users', authenticateToken, controller.getUsers);
+router.get('/user/:id', authenticateToken, controller.getUser);
+router.patch('/user/:id', authenticateToken, controller.updateUser);
+router.delete('/user/:id', authenticateToken, controller.deleteUser);
 
 export = router;
